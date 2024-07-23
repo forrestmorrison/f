8bf528a8ea76b70ea852dc99b0cb846b9bf9071f
Forrest Morrison <forrestmorrison@gmail.com>
2024-07-22 19:04:24 -0500

    Added React Router and Routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import Home from "./pages/Home";
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Messages from './pages/Messages';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Nav />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/messages' element={<Messages />} />
+        <Route path='/notifications' element={<Notifications />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
