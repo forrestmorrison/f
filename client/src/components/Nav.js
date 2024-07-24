afb686137878c95feab2d43a56b5d0a80d55d6b1
Forrest Morrison <forrestmorrison@gmail.com>
2024-07-23 22:02:07 -0500

    added react icons to nav items

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom'
 import FLogo from '../images/f-logo.png'
+import { GoBell, GoHomeFill, GoMail, GoPerson } from 'react-icons/go'
+import { FaSearch } from 'react-icons/fa'
 
 const Nav = () => {
   return (
     <div className='nav'>
       <img src={FLogo} alt='' className='f-logo'/>
       <ul>
-        <li><Link to='/explore'>Explore</Link></li>
-        <li><Link to='/notifications'>Notifications</Link></li>
-        <li><Link to='/messages'>Messages</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
+        <li><GoHomeFill className='icon' /><Link to='/'>Home</Link></li>
+        <li><FaSearch className='icon' /><Link to='/explore'>Explore</Link></li>
+        <li><GoBell className='icon' /><Link to='/notifications'>Notifications</Link></li>
+        <li><GoMail className='icon' /><Link to='/messages'>Messages</Link></li>
+        <li><GoPerson className='icon' /><Link to='/profile'>Profile</Link></li>
       </ul>
     </div>
   )
